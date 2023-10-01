8a7340d99332e27b3125bf15e41caeb0d6822c86
AnasB <rafa0anas@gmail.com>
2023-10-01 14:43:45 +0200

    added more algorithms and tests 2

---
+++ algoQuestions/src/main/java/org/training/BubbleSort.java
@@ -0,0 +1,31 @@
+package org.training;
+
+import static org.utils.SortUtils.greater;
+import static org.utils.SortUtils.swap;
+
+class BubbleSort {
+
+    /**
+     * Implements generic bubble sort algorithm.
+     *
+     * @param array the array to be sorted.
+     * @param <T> the type of elements in the array.
+     * @return the sorted array.
+     */
+
+    public <T extends Comparable<T>> T[] sort(T[] array) {
+        for (int i = 1, size = array.length; i < size; ++i) {
+            boolean swapped = false;
+            for (int j = 0; j < size - i; ++j) {
+                if (greater(array[j], array[j + 1])) {
+                    swap(array, j, j + 1);
+                    swapped = true;
+                }
+            }
+            if (!swapped) {
+                break;
+            }
+        }
+        return array;
+    }
+}
